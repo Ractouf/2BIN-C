9989b77ba329e9d8f0d7d51668f29d217b4cfb24
François Vandeputte <francois.vandeputte@student.vinci.be>
2022-12-08 15:46:18 +0100

    fin cours

--- TP6/3.c
+++ TP6/3.c
@@ -1,7 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int* prime_numbers (int entier, int* sz) {  
+	int size = 0;
+
+	for (int i = entier; i > 1; i--) {
+		for (int j = 2; j < i; j++) {
+		    if (i % j == 0) {
+		    	break;
+		    }
+		    if (j == i - 1) {
+		    	size ++;
+		    }
+	  	}	
+	}
+
+	int* tableau = malloc(size * sizeof(int));
+	if (tableau == NULL) {
+		return NULL;
+	}
+
+	int k = 0;
+
+	for (int i = entier; i > 1; i--) {
+		for (int j = 2; j < i; j++) {
+		    if (i % j == 0) {
+		    	break;
+		    }
+		    if (j == i - 1) {
+		    	tableau[k] = i;
+		    	k ++;
+		    }
+	  	}	
+	}
+
+	*sz = size;
+	return tableau;
+}
+
 int main (int argc, char *argv[]) {
 	
+	int entier = 0;
+
+	do {
+		printf("Entrez un entier : ");
+		if(!scanf("%d", &entier)) {
+			printf("Des entiers silteplé\n");
+			break;
+		}
+		if (entier < 2) {
+			printf("Entier doit être >= 2");
+		}
+	} while (entier < 2);
 	
+	int szr;
+	int* tableau = prime_numbers(entier, &szr);
+
+	for (int i = 0; i < szr; i ++) {
+		printf("%d\n", tableau[i]);
+	}
 }
