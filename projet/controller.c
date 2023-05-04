aefbe9b822737308db5bed564da629f09bd27553
Ractouf <francois.vandeputte@student.vinci.be>
2023-05-04 12:05:16 +0200

    child process

--- projet/controller.c
+++ projet/controller.c
@@ -1,19 +1,9 @@
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <sys/types.h>
 
 #include "utils_v2.h"
 
 #define LOCAL_HOST "127.0.0.1"
 #define SERVER_PORT 9090
-#define VAL 7
 
 int main(int argc, char *arg[]) {
   // socket creation
@@ -23,9 +13,9 @@ int main(int argc, char *arg[]) {
   sconnect(LOCAL_HOST, SERVER_PORT, sockfd);
   
   // write message to server
-  int val = VAL;
-  printf("Client envoie la valeur 7 au serveur\n");
-  swrite(sockfd,&val,sizeof(int));
+  char* str = "ls -l";
+  printf("Client envoie le string au serveur\n");
+  swrite(sockfd, str, strlen(str));
   
   // close socket
   sclose(sockfd);
--- projet/zombie.c
+++ projet/zombie.c
@@ -1,16 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
+#include <fcntl.h>
 
 #include "utils_v2.h"
 
 #define BACKLOG 5
 #define PORT 9090
+
+#define TAILLE 255
+#define BASH "programme_inoffensif.sh"
+
+void child (void *argv) {
+  int *pipefd = argv;
+
+  sclose(pipefd[1]);
+
+  int fd = open(BASH, O_WRONLY | O_TRUNC | O_CREAT, 0700);
+  checkNeg(fd, "Error opening file");
+
+  char* shebang = "#!/bin/bash\n";
+  swrite(fd, shebang, strlen(shebang));
+
+  char bufferPipeRd[TAILLE];
+  int nbCharRd = sread(pipefd[0], bufferPipeRd, TAILLE);
+
+  swrite(fd, bufferPipeRd, nbCharRd);
+}
 
 int main(int argc, char *arg[]) {
   // socket creation
@@ -23,10 +37,27 @@ int main(int argc, char *arg[]) {
   int newsockfd = saccept(sockfd);
   
   // read message from client
-  int val;
-  sread(newsockfd, &val, sizeof(int));
-  printf("Valeur reçue : %i\n", val);
+  char bufRd[TAILLE];
+  int nbCharRd = sread(newsockfd, bufRd, TAILLE);
+  printf("Valeur reçue : %s\n", bufRd);
   
+  int pipefd[2];
+  spipe(pipefd);
+
+  pid_t childId = fork_and_run1(child, pipefd);
+
+  printf("%d\n", childId);
+  printf("parent\n");
+
+  sclose(pipefd[0]);
+
+  while (nbCharRd > 0) {
+    swrite(pipefd[1], bufRd, nbCharRd);
+    nbCharRd = sread(0, bufRd, TAILLE);
+  }
+
+  sclose(pipefd[1]);
+
   // close connection client
   sclose(newsockfd);
   // close listen socket
